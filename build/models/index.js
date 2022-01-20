c1f5ef1f4ad560ae23ed806aa5c5b7141745029e
Michael Aidoo <aidoomichael20@gmail.com>
2022-01-20 08:44:44 +0000

    compilation error fix

--- build/models/index.js
+++ build/models/index.js
@@ -6,22 +6,17 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
+console.dir("-----------------------ENVIRONMENT----------------------: " + config.password);
 const db = {};
 const cls = require('cls-hooked');
 const namespace = cls.createNamespace('metacaretestnamespace');
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+console.log(process.env.CLEARDB_DATABASE_URL);
+if (process.env.CLEARDB_DATABASE_URL && process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize('mysql://b28c7e0f1608c2:db2987cd@us-cdbr-east-05.cleardb.net/heroku_42fbf93b3d1d2b3?reconnect=true'); // for production
 }
 else {
-    console.log(process.env.CLEARDB_DATABASE_URL);
-    if (process.env.CLEARDB_DATABASE_URL && process.env.NODE_ENV === 'production') {
-        sequelize = new Sequelize('mysql://b28c7e0f1608c2:db2987cd@us-cdbr-east-05.cleardb.net/heroku_42fbf93b3d1d2b3?reconnect=true'); // for production
-    }
-    else {
-        sequelize = new Sequelize(config.database, config.username, config.password, config);
-    }
-    // sequelize = new Sequelize('mysql://b1c8b81cf345a7:2dd9d53b@us-cdbr-east-04.cleardb.com/heroku_540ebb5287e6a03', config)
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 fs
     .readdirSync(__dirname)
