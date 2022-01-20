96c0c2b1cde0320fc52d1edb3bac762f77b1d7c6
Michael Aidoo <aidoomichael20@gmail.com>
2022-01-20 05:17:48 +0000

    compilation error fix

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,6 +5,9 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
+
+console.log("-----------------------ENVIRONMENT----------------------: " + env)
+
 const config = require(__dirname + '/../config/config.js')[env]
 const db: any = {}
 
