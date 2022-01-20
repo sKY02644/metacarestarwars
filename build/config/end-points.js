26d841d6290f5ebd42ca738c40416c259e74dd4f
Michael Aidoo <aidoomichael20@gmail.com>
2022-01-20 15:43:26 +0000

    added a mods folder to fix models not instantiating

--- build/config/end-points.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENDPOINT_URL = void 0;
-var ENDPOINT_URL;
-(function (ENDPOINT_URL) {
-    ENDPOINT_URL["CHARACTER"] = "https://swapi.py4e.com/api/people/";
-    ENDPOINT_URL["MOVIE"] = "https://swapi.py4e.com/api/films/";
-    ENDPOINT_URL["COMMENT"] = "http://localhost:3000/comments/";
-})(ENDPOINT_URL = exports.ENDPOINT_URL || (exports.ENDPOINT_URL = {}));
--- build/error-logs/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const winston = require('winston');
-exports.default = winston.createLogger({
-    transports: [
-        new winston.transports.File({ filename: './error-logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: './error-logs/combined.log' }),
-    ]
-});
